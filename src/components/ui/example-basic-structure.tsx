9157b64ca80ab7faeef1fab2cabcbdd0c5888e97
jhean <brizadaojhean@gmail.com>
2023-12-19 23:52:44 -0300

    :recycle: refactor: adicionando  component <GenerateEmblaSliders/>

--- src/components/ui/example-basic-structure.tsx
+++ src/components/ui/example-basic-structure.tsx
@@ -1,50 +1,14 @@
-import Img1 from '@/components/assets/img-1.jpg'
-import Img2 from '@/components/assets/img-2.jpg'
-import Img3 from '@/components/assets/img-3.jpg'
-import Image from 'next/image'
+import { EmblaContainer } from '../embla/embla'
 import EmblaWrapper from '../embla/embla-wrapper'
+import GenerateEmblaSliders from '../utils/generateEmblaSliders'
 
 export const EmblaCarousel = () => {
   return (
     <div className="w-full max-w-3xl">
       <EmblaWrapper>
-        <div className="embla__container">
-          <div className="embla__slide">
-            <div className="relative h-56 w-full overflow-hidden rounded">
-              <Image
-                src={Img1}
-                alt="descriptionImage"
-                fill
-                sizes="100vw"
-                className="object-cover"
-              />
-            </div>
-          </div>
-          <div className="embla__slide">
-            {' '}
-            <div className="relative h-56 w-full overflow-hidden rounded">
-              <Image
-                src={Img2}
-                alt="descriptionImage"
-                fill
-                sizes="100vw"
-                className="object-cover"
-              />
-            </div>
-          </div>
-          <div className="embla__slide">
-            {' '}
-            <div className="relative h-56 w-full overflow-hidden rounded">
-              <Image
-                src={Img3}
-                alt="descriptionImage"
-                fill
-                sizes="100vw"
-                className="object-cover"
-              />
-            </div>
-          </div>
-        </div>
+        <EmblaContainer>
+          <GenerateEmblaSliders />
+        </EmblaContainer>
       </EmblaWrapper>
     </div>
   )
