43751a2a19cf439547f4b8ff2cbb165262c6a4b4
jhean <brizadaojhean@gmail.com>
2023-12-21 19:49:07 -0300

    :sparkles:	feat: custom hook useEmblaSliderPaginationNavigation para slider que precisa de paginação e navegação.

---
+++ src/components/ui/useEmblaSliderPaginationNavigation.ts
@@ -0,0 +1,56 @@
+import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react'
+import { useCallback, useEffect, useState } from 'react'
+import { useNavigation } from './useNavigation'
+
+export const useEmblaSliderPaginationNavigation = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([])
+  const {
+    nextBtnDisabled,
+    setNextBtnDisabled,
+    scrollNext,
+    prevBtnDisabled,
+    setPrevBtnDisabled,
+    scrollPrev,
+  } = useNavigation(emblaApi)
+
+  const scrollTo = useCallback(
+    (index: number) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi],
+  )
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList())
+  }, [])
+
+  const onSelect = useCallback(
+    (emblaApi: EmblaCarouselType) => {
+      setSelectedIndex(emblaApi.selectedScrollSnap())
+      setPrevBtnDisabled(!emblaApi.canScrollPrev())
+      setNextBtnDisabled(!emblaApi.canScrollNext())
+    },
+    [setNextBtnDisabled, setPrevBtnDisabled],
+  )
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    onInit(emblaApi)
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onInit)
+    emblaApi.on('reInit', onSelect)
+    emblaApi.on('select', onSelect)
+  }, [emblaApi, onInit, onSelect])
+
+  return {
+    emblaRef,
+    prevBtnDisabled,
+    nextBtnDisabled,
+    selectedIndex,
+    scrollSnaps,
+    scrollPrev,
+    scrollNext,
+    scrollTo,
+  }
+}
