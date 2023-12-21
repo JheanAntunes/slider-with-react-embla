af0cd6c436d2819dcf46fb06a91121c77e102cc1
jhean <brizadaojhean@gmail.com>
2023-12-21 19:15:01 -0300

    :lipstick:	feat: import dos slider, slider com auto-scroll, navigation, pagination e navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { ExampleBasicSliderWithNavigation } from '@/components/ui/example-basic-slider-with-navigation'
 import { EmblaCarousel } from '@/components/ui/example-basic-structure'
-import Image from 'next/image'
+import ExampleBasicStructureAutoScroll from '@/components/ui/example-basic-structure-auto-scroll'
+import ExampleEmblaSliderNavigationPagination from '@/components/ui/example-embla-slider-navigation-pagination'
 
 export default function Home() {
   return (
-   <main>
-    <EmblaCarousel />
-   </main>
+    <main className="container flex flex-col gap-8 py-8">
+      <EmblaCarousel />
+      <ExampleBasicStructureAutoScroll />
+      <ExampleBasicSliderWithNavigation />
+      <ExampleEmblaSliderNavigationPagination />
+    </main>
   )
 }
