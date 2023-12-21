3e7b44189086fa6302ce6edf92c2244cad5cf63d
jhean <brizadaojhean@gmail.com>
2023-12-21 19:11:28 -0300

    :recycle: refactor: adicionando type de options para autoplay e options

--- src/components/embla/embla-wrapper-auto-scroll.tsx
+++ src/components/embla/embla-wrapper-auto-scroll.tsx
@@ -2,11 +2,30 @@
 import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 import React from 'react'
+
+type MyAutoplayOptions = {
+  delay: number
+  jump: boolean
+  playOnInit: boolean
+  stopOnFocusIn: boolean
+  stopOnInteraction: boolean
+  stopOnMouseEnter: boolean
+  stopOnLastSnap: boolean
+  rootNode: ((emblaRoot: HTMLElement) => HTMLElement | null) | null
+}
+
 function EmblaWrapperAutoScroll({
   children,
   ...restProps
 }: React.ComponentProps<'div'>) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
+  const autoplayOptions: Partial<MyAutoplayOptions> = {
+    delay: 4000,
+    stopOnInteraction: false,
+    rootNode: (emblaRoot) => emblaRoot.parentElement,
+  }
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [
+    Autoplay(autoplayOptions),
+  ])
 
   React.useEffect(() => {
     if (emblaApi) {
