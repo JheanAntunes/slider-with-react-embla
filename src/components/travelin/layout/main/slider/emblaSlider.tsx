13f4af12c7427a7a633d45ead737254710166860
jhean <brizadaojhean@gmail.com>
2023-12-22 19:43:18 -0300

    :recycle: refactor: utilizando custom hook useEmblaSliderPaginationNavigation

--- src/components/travelin/layout/main/slider/emblaSlider.tsx
+++ src/components/travelin/layout/main/slider/emblaSlider.tsx
@@ -3,26 +3,19 @@
 import ImgExample1 from '@/components/assets/img-1.jpg'
 import ImgExample2 from '@/components/assets/img-2.jpg'
 import ImgExample3 from '@/components/assets/img-3.jpg'
+import { useEmblaSliderPaginationNavigation } from '@/components/ui/useEmblaSliderPaginationNavigation'
 import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
-import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
-import { useCallback } from 'react'
 
 function EmblaSlider() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({
-    loop: true,
-    skipSnaps: true,
-  })
-
-  const handleClickBtnPrev = useCallback(() => {
-    if (!emblaApi) return
-    emblaApi.scrollPrev()
-  }, [emblaApi])
-
-  const handleClickBtnNext = useCallback(() => {
-    if (!emblaApi) return
-    emblaApi.scrollNext()
-  }, [emblaApi])
+  const {
+    emblaRef,
+    scrollNext,
+    scrollPrev,
+    scrollSnaps,
+    scrollTo,
+    selectedIndex,
+  } = useEmblaSliderPaginationNavigation()
 
   return (
     <div className="container absolute -top-8">
@@ -59,17 +52,26 @@ function EmblaSlider() {
           </div>
         </div>
         <div className="flex items-center justify-center gap-8">
-          <button className="h-10 w-10 text-white" onClick={handleClickBtnPrev}>
+          <button className="h-10 w-10 text-white" onClick={scrollPrev}>
             <ArrowLeftIcon />
           </button>
-          {/* <div className="flex gap-8">
-            <div className="">01</div>
-            <div className="">02</div>
-            <div className="">03</div>
-          </div> */}
+          <div className="flex gap-8">
+            {scrollSnaps.map((_, index) => (
+              <button
+                key={index}
+                className={`h-5 w-5 rounded-full ${
+                  index === selectedIndex ? 'bg-orange-500' : 'bg-orange-500/35'
+                }`}
+                onClick={() => {
+                  scrollTo(index)
+                  return
+                }}
+              />
+            ))}
+          </div>
           <button
             className="h-10 w-10 rounded-full text-white"
-            onClick={handleClickBtnNext}
+            onClick={scrollNext}
           >
             <ArrowRightIcon />
           </button>
